39a7d650ce7db5962b34a40d90d3da3aa4996f63
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-08-30 13:14:29 +0100

    adding window onLoad function

--- Intro_to_JS/test3.js
+++ Intro_to_JS/test3.js
@@ -1,20 +1,31 @@
-// setting both the button and content into variables
+function setUpEvents() {
+    // setting both the button and content into variables
+    let content = document.getElementById('content');
+    let button = document.getElementById('show-more');
 
-let content = document.getElementById('content');
-let button = document.getElementById('show-more');
-
-button.onclick = function() {
-    // check if the box is open then we want to shrink it otherwise expand it
-    if (content.className == "open") {
-        // shrink the box
-        content.className = " ";
-        button.innerHTML = "Show  more";
+    button.onclick = function() {
+        // check if the box is open then we want to shrink it otherwise expand it
+        if (content.className == "open") {
+            // shrink the box
+            content.className = " ";
+            button.innerHTML = "Show  more";
 
 
-    } else {
-        // expand the box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-    }
+        } else {
+            // expand the box
+            content.className = "open";
+            button.innerHTML = "Show Less";
+        }
+
+    };
+
+};
+
+
+
+window.onload = function() {
+    // after everything in the HTML has loaded then run this function
+    // this makes sure that we don't fire any JS until the window has fully loadedm
+    setUpEvents();
 
 };
