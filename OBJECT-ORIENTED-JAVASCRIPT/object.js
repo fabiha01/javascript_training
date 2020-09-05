359d72bc19c9a1fe87367812d6cd73a78cc2279f
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-05 21:00:36 +0100

    change object to class with constructor

--- OBJECT-ORIENTED-JAVASCRIPT/object.js
+++ OBJECT-ORIENTED-JAVASCRIPT/object.js
@@ -1,13 +1,19 @@
-let userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login() {
-        console.log(this.email, 'has logged in')
-    },
-    logout() {
-        console.log(this.email, 'has logged out.')
-    }
-    
-};
+class User {
+    // this is now a class
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
 
-userOne.name = 'Yoshi';
+    }
+}
+
+const userOne = new User('ryu@ninjas.com', 'Ryu');
+const userTwo = new User('Yoshi@mariocorp.com', 'Yoshi');
+
+console.log(userOne);
+console.log(userTwo);
+
+// the 'new' keyword
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
