ddc658cd9be1fa64196b5485c6284a37fc79b503
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-06 10:16:56 +0100

    chaining methods

--- OBJECT-ORIENTED-JAVASCRIPT/object.js
+++ OBJECT-ORIENTED-JAVASCRIPT/object.js
@@ -3,15 +3,34 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this.score = 0;
 
     }
+    login() {
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+    logout() {
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore() {
+        this.score ++;
+        console.log(this.email, 'score is now', this.score);
+        // return this - the instance of the object
+        // that particular user
+        return this;
+
+    }
+
 }
 
 const userOne = new User('ryu@ninjas.com', 'Ryu');
 const userTwo = new User('Yoshi@mariocorp.com', 'Yoshi');
 
-console.log(userOne);
-console.log(userTwo);
+// chaining methods
+userOne.login().updateScore().updateScore().logout();
+
 
 // the 'new' keyword
 // - creates a new empty object {}
