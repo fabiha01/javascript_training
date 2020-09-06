80ff14a79799c0331dc5a833f547213704125db5
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-06 11:19:27 +0100

    class inheritance

--- OBJECT-ORIENTED-JAVASCRIPT/object.js
+++ OBJECT-ORIENTED-JAVASCRIPT/object.js
@@ -25,8 +25,34 @@ class User {
 
 }
 
+// use extends to be able to base the class from another class
+// it will still include the properties and methods as above
+// but you can add on top of it
+
+// created a new class called Admin
+// it has inherited the class from User
+class Admin extends User {
+    deleteUser(user) {
+        users = users.filter(u => {
+            // of the email of the array is not equal to
+            // the user that we passed in
+            // then we want to filter it out
+            return u.email !== user.email
+        });
+    }
+
+
+}
+
 const userOne = new User('ryu@ninjas.com', 'Ryu');
 const userTwo = new User('Yoshi@mariocorp.com', 'Yoshi');
+const admin = new Admin('fabiha@ninjas.com', 'Fabiha');
+
+let users = [userOne, userTwo, admin];
+
+// admin.deleteUser(userOne);
+
+console.log(users);
 
 // chaining methods
 userOne.login().updateScore().updateScore().logout();
