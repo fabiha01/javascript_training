a63b9c07cdd0f8111bfc955ae4774bb8090612cf
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-03-03 16:03:54 +0000

    Fix: create the invoice class

---
+++ TS/src/classes/Invoice.ts
@@ -0,0 +1,22 @@
+import { HasFormatter } from '../interfaces/HasFormatter.js';
+
+// classes
+export class Invoice implements HasFormatter {
+  // readonly client: string;
+  // private details: string;
+  // amount: number;
+
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number,
+  ) {
+
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+// export default Invoice;
