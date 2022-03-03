ff5cbb46961ab7d49c0bbbf42a7a871611a86971
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-03-03 16:04:41 +0000

    Fix: payment class

---
+++ TS/public/classes/Payment.js
@@ -0,0 +1,14 @@
+// classes
+export class Payment {
+    // readonly client: string;
+    // private details: string;
+    // amount: number;
+    constructor(recipient, details, amount) {
+        this.recipient = recipient;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.recipient} is owed £${this.amount} for ${this.details}`;
+    }
+}
---
+++ TS/src/classes/Payment.ts
@@ -0,0 +1,21 @@
+import { HasFormatter } from '../interfaces/HasFormatter.js';
+
+// classes
+export class Payment implements HasFormatter {
+  // readonly client: string;
+  // private details: string;
+  // amount: number;
+
+  constructor(
+    readonly recipient: string,
+    private details: string,
+    public amount: number,
+  ) {
+
+  }
+
+  format() {
+    return `${this.recipient} is owed £${this.amount} for ${this.details}`;
+  }
+}
+
